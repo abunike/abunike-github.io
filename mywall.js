ea899ae559c8fc773fadce3532da15355623f7b2
Nweke Ifeabunike <74120277+abunike@users.noreply.github.com>
2020-11-09 03:29:12 +0100

    Add files via upload

---
+++ mywall.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("topnav");
+    if (x.className === "sidenav") {
+        x.className += " responsive";
+    } else {
+        x.className = "sidenav";
+    }
+}
